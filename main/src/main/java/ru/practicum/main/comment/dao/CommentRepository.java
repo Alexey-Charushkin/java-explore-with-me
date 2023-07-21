90bd099cf9ffec5caafc645173b1e1e0ee7cf05f
Alexey-Charushkin <113549089+Alexey-Charushkin@users.noreply.github.com>
2023-07-21 18:37:39 +0300

    Update CommentRepository.java

--- main/src/main/java/ru/practicum/main/comment/dao/CommentRepository.java
+++ main/src/main/java/ru/practicum/main/comment/dao/CommentRepository.java
@@ -75,46 +75,4 @@ public interface CommentRepository extends JpaRepository<Comment, Integer> {
     List<Comment> findAllByEventIdAndCommentStateAndCreatedIsAfter(
             Integer userId, CommentState state, LocalDateTime localDateTime, Pageable pageable
     );
-
-
-//    findAllByUserIdAndRequestParam(Integer userId, String state, String start, String end,
-//                                   Integer from, Integer size)
-
-//    @Query("SELECT c " +
-//            "FROM Comment AS c " +
-//            "WHERE " +
-//            ":text IS NULL OR LOWER(c.text) LIKE LOWER(CONCAT('%', :text, '%')) " +
-//            "AND (:eventId IS NULL OR c.event.id = :eventId) " +
-//            "AND (CAST(:rangeStart AS date) IS NULL OR c.created >= :rangeStart) " +
-//            "AND (CAST(:rangeEnd AS date) IS NULL OR c.created <= :rangeEnd) " +
-//            "AND c.commentState = :commentState " +
-//            "ORDER BY :sort")
-//    List<Comment> getComments(@Param("eventId") Long eventId,
-//                              @Param("text") String text,
-//                              @Param("rangeStart") LocalDateTime rangeStart,
-//                              @Param("rangeEnd") LocalDateTime rangeEnd,
-//                              @Param("sort") String sort,
-//                              @Param("commentState") CommentState commentState,
-//                              Pageable pageable);
-//
-//
-//    @Query("SELECT c " +
-//            "FROM Comment AS c " +
-//            "WHERE " +
-//            "c.user.id = :userId " +
-//            "AND (:text IS NULL OR LOWER(c.text) LIKE LOWER(CONCAT('%', :text, '%'))) " +
-//            "AND (:eventId IS NULL OR c.event.id = :eventId) " +
-//            "AND (CAST(:rangeStart AS date) IS NULL OR c.created >= :rangeStart) " +
-//            "AND (CAST(:rangeEnd AS date) IS NULL OR c.created <= :rangeEnd) " +
-//            "AND c.commentState = :commentState " +
-//            "ORDER BY :sort")
-//    List<Comment> getCommentsByAdmin(@Param("userId") Long userId,
-//                                     @Param("eventId") Long eventId,
-//                                     @Param("text") String text,
-//                                     @Param("rangeStart") LocalDateTime startTime,
-//                                     @Param("rangeEnd") LocalDateTime endTime,
-//                                     @Param("sort") String sort,
-//                                     @Param("commentState") CommentState commentState,
-//                                     PageRequest pageable);
-//}
 }
